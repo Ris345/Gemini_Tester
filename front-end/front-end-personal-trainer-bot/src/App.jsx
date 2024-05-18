a9eb72f74a93f32a03f94ae25d5b820fa329d622
Dorjee <rishav345@gmail.com>
2024-05-18 11:40:21 -0400

    Working through the front-end and back-end connection

--- front-end/front-end-personal-trainer-bot/src/App.jsx
+++ front-end/front-end-personal-trainer-bot/src/App.jsx
@@ -2,18 +2,48 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setuserData] = useState('')
+  const [incomingData, setIncomingData] = useState(null)
+  async function callBot() {
+    const userInput = null
+    console.log(userData)
+
+    try {
+
+      const response = await fetch('/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userInput: userData }),
+      });
+
+      const data = await response.json();
+      console.log(data)
+      const botMessage = data.response;
+      console.log(botMessage)
+      // Add chat message to the chat history
+
+      // Scroll to the bottom of the chat history
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle errors gracefully, e.g., display an error message to the user
+    }
+  }
 
   return (
     <div>
       <div className=" h-screen  w-6/12 border border-black">
         <h1>Jinko Bot</h1>
+        <p>{userData}</p>
     </div>
     <div className="h-20 border w-6/12  border-black">
-        <button>Ask</button>
+        <button onClick={callBot}>Ask</button>
+        <input type="text" placeholder='text input here' onChange={e => setuserData(e.target.value)}/> 
         </div>
   </div>
   )
 }
+
 
 export default App
